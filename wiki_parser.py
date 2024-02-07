7c0223ce186fd3ec8c77db90e56232def4c762f4
emperornao <ansicpp2020@gmail.com>
2024-02-07 20:00:29 +0300

    Fix

--- wiki_parser.py
+++ wiki_parser.py
@@ -1,4 +1,5 @@
 import os
+import json
 from collections import deque
 import requests
 from bs4 import BeautifulSoup
@@ -6,40 +7,34 @@ from bs4 import BeautifulSoup
 
 class WikiParser:
     LINKS_CAP = 100_000
+    BLACKLIST = ["Файл:", "Википедия:", "Категория:", "некорректный url"]
 
     def __init__(self,
                  file_dir,
-                 words_buffer_size=10_000,
                  max_words=1_000_000):
-        self.words_buffer_size = words_buffer_size
         self.max_words = max_words
         self.file_dir = file_dir
 
     @staticmethod
-    def _filename(number_of_blocks):
-        return f"{number_of_blocks}.txt"
-
-    @staticmethod
-    def _load_to_file(words, max_size, file_path):
-        s = "\n".join(words[:max_size])
-        with open(file_path, "w") as fp:
-            print(s, file=fp)
-        return words[max_size:]
+    def _filename(idx):
+        return f"{idx}.txt"
 
     @staticmethod
     def _parse(content):
         soup = BeautifulSoup(content, 'html.parser')
 
         body = soup.find(id="bodyContent")
+        title = soup.find('title')
 
         links = soup.find(id="bodyContent").find_all("a")
         wiki_links = list(
             map(lambda x: x["href"],
             filter(lambda x: x.get("href") and x["href"].find("/wiki/") != -1, links))
         )
-        words = list(filter(lambda x: x and x.isalpha(), map(lambda x: x.strip().lower(), body.text.strip().split())))
+
         return {
-            "words": words,
+            "title": title.text,
+            "body": body.text,
             "links": wiki_links
         }
 
@@ -53,15 +48,16 @@ class WikiParser:
         visited = set()
 
         number_of_words = 0
-        number_of_blocks = 0
-        words = []
+        kv = {}
 
         queue = deque()
         queue.append(start_link)
-        visited.add(start_link)
 
         while queue and number_of_words < self.max_words:
             url = queue.popleft()
+            if url in visited:
+                continue
+            print(url)
             visited.add(url)
             try:
                 response = requests.get(url=url)
@@ -72,25 +68,32 @@ class WikiParser:
                 continue
 
             if verbose:
-                print(f"{len(queue)} in queue, {number_of_blocks} prepared blocks, "
+                print(f"{len(queue)} in queue, {len(kv)} number of docs, "
                       f"{number_of_words} / {self.max_words} status")
 
             parsed = WikiParser._parse(response.content)
-            words.extend(parsed["words"])
-            number_of_words += len(parsed["words"])
+
+            stop = False
+            for blackpart in WikiParser.BLACKLIST:
+                stop = stop or blackpart in parsed['title']
+            if stop:
+                continue
+
+            body = parsed['body']
+            words = list(filter(lambda x: x and x.isalpha(), map(lambda x: x.strip().lower(), body.strip().split())))
+            number_of_words += len(words)
 
             for link in parsed["links"]:
                 if link in visited or len(queue) >= WikiParser.LINKS_CAP:
                     continue
                 queue.append("https://ru.wikipedia.org" + link)
 
-            while len(words) >= self.words_buffer_size:
-                words = WikiParser._load_to_file(
-                    words,
-                    self.words_buffer_size,
-                    os.path.join(self.file_dir, WikiParser._filename(number_of_blocks))
-                )
-                number_of_blocks += 1
+            with open(os.path.join(self.file_dir, WikiParser._filename(len(kv))), "w") as fw:
+                print(body, file=fw)
+            kv[WikiParser._filename(len(kv))] = parsed['title']
+
+        with open(os.path.join(self.file_dir, "index.json"), "w") as index:
+            json.dump(kv, index)
 
 
 if __name__ == "__main__":
